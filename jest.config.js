97e5d2961f347a12ed67cc09d9011b070f91b2f9
GorbunovS <obes1stas1@gmail.com>
2025-04-12 23:59:05 +0300

    Стабильная версия

--- jest.config.js
+++ jest.config.js
@@ -6,12 +6,12 @@ export default {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
   },
   testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
-  globals: {
-    'ts-jest': {
-      useESM: true,
-    },
-  },
 }
